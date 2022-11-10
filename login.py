d41cea005957854167d54690c0822e9940c35f26
clb <1796390642@qq.com>
2022-11-10 21:38:50 +0800

    增加登录功能、优化经营状况异常判断

--- login.py
+++ login.py
@@ -1,19 +1,14 @@
-from time import sleep
-
+import requests
 from selenium import webdriver
-from urllib.parse import quote
 import pickle
 
 option = webdriver.ChromeOptions()
 # option.add_argument("headless")
 browser = webdriver.Chrome(chrome_options=option)
 browser.get("https://www.tianyancha.com/company/23402373")
-
+# pickle.dump(browser.get_cookies(),open("cookie.pkl","wb"))
 cookies = pickle.load(open("cookie.pkl", "rb"))
 for cookie in cookies:
     browser.add_cookie(cookie)
 browser.get("https://www.tianyancha.com/company/23402373")
-
-temp = browser.find_element_by_id("inverst-table")
-temp = temp.find_element_by_xpath("./div/div[@class='table-footer']/div/div/div/div[2]").click()
 pass
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import pickle
 import sys
 from time import sleep
 from urllib.parse import quote
@@ -11,12 +12,12 @@ from tqdm import tqdm
 class crawler:
     def __init__(self):
         self.all_companys = pd.read_excel('dataset/data.xlsx')
-        self.key_words = self.all_companys['企业名称'].to_list()[50:60]  # 在这更改处理企业范围
-        # self.key_words = ['万源市金桥建材有限责任公司']
-        # key_words = ['七台河矿业精煤集团有限公司']
+        self.key_words = self.all_companys['企业名称'].to_list()[70:100]  # 在这更改处理企业范围
+        # self.key_words = ['一汽山东汽车改装厂']
         self.exist = []  # 能否匹配到网址
         self.num = 0
         self.DealTimeOut_max_times = 0
+        self.DealUnKnown_max_times = 0
         self.hrefs = []  # 匹配到的企业名称和网址(字典格式存储)
         self.unable_1 = []  # 查找不到网址
         self.unable_2 = []  # 无股东信息
@@ -25,13 +26,21 @@ class crawler:
         self.unable_5 = []  # 未知原因未被获取
 
         option = webdriver.ChromeOptions()
-        option.add_argument("headless")  # 取消注释可以不显示弹出的chrome浏览器
+        # option.add_argument("headless")  # 注释可以显示chrome浏览器
         option.add_argument('no-sandbox')
         option.add_argument(
             "user-agent='Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36'")
         # 本地运行用第一行，linux服务器运行用第二行
-        # self.browser = webdriver.Chrome(chrome_options=option)
-        self.browser = webdriver.Chrome(options=option, executable_path='/root/chromedriver')
+        self.browser = webdriver.Chrome(chrome_options=option)
+        self.browser.maximize_window()
+        # self.browser = webdriver.Chrome(options=option, executable_path='/root/chromedriver')
+        self.LogIn()
+
+    def LogIn(self):
+        self.browser.get("https://www.tianyancha.com/company/23402373")
+        cookies = pickle.load(open("cookie.pkl", "rb"))
+        for cookie in cookies:
+            self.browser.add_cookie(cookie)
 
     def GetURL(self):
         for key_word in tqdm(self.key_words, desc="获取网址", file=sys.stdout):
@@ -40,9 +49,9 @@ class crawler:
             self.browser.implicitly_wait(1)
 
             company = self.browser.find_element_by_xpath("//*[@class='index_name__qEdWi']/a")
-            LogOut = company.find_element_by_xpath("../../div[2]")
-            if LogOut.text == "注销":
-                print("已注销:", key_word)
+            LogOut = company.find_element_by_xpath("../../div[2]").get_attribute('class')
+            if 'normal' not in LogOut:
+                print("经营状况异常:", key_word)
                 self.exist.append(0)
                 self.unable_3.append(key_word)
                 continue
@@ -109,8 +118,10 @@ class crawler:
 
             # 在上方索引栏定位股东信息
             try:
-                self.browser.find_element_by_xpath(
+                gudong_title = self.browser.find_element_by_xpath(
                     '//div[@class="index_tag-nav-root__DyEBq"]/a[contains(text(),"股东信息")]')
+                total_len = int(gudong_title.find_element_by_xpath("./span").text)
+                pass
             except:  # 如果在索引栏没有找到说明无股东信息
                 print("无股东信息:", href['company_name'])
                 self.unable_2.append(href['company_name'])
@@ -120,16 +131,26 @@ class crawler:
             gudongs = []  # 股东
             chigubilis = []  # 持股比例
 
-            GuDong = self.browser.find_elements_by_xpath("//div[@data-dim='holder']/div[2]/div/table/tbody/tr")
-            WebDriverWait(self.browser, 10)
-            GuDong_size = len(GuDong)
+            # GuDong_size = len(GuDong)
+            page = int(total_len / 20) + 1
+            current_page = 0
             try:
-                for item in GuDong:
+                while page != current_page:
+                    if current_page != 0:
+                        temp = self.browser.find_element_by_xpath(
+                            f"//div[@data-dim='holder']/div[2]/div/div[@class='table-footer']/div/div/div/div[{current_page + 1}]")
+                        self.browser.execute_script("arguments[0].click();", temp)
+                    sleep(1)
+                    self.GuDong = self.browser.find_elements_by_xpath("//div[@data-dim='holder']/div[2]/div/table/tbody/tr")
                     WebDriverWait(self.browser, 10)
-                    data = item.find_elements_by_xpath("./td")
-                    WebDriverWait(self.browser, 10)
-                    gudongs.append(data[1].find_element_by_xpath("./div/div[2]/div/div/a").text)
-                    chigubilis.append(data[2].text)
+                    for item in self.GuDong:
+                        WebDriverWait(self.browser, 10)
+                        data = item.find_elements_by_xpath("./td")
+                        WebDriverWait(self.browser, 20).until(
+                            lambda diver: data[1].find_element_by_xpath("./div/div[2]/div/div/a"))
+                        gudongs.append(data[1].find_element_by_xpath("./div/div[2]/div/div/a").text)
+                        chigubilis.append(data[2].text)
+                    current_page += 1
             except:
                 print("超时未获取:", href['company_name'])
                 self.unable_4.append(href)
@@ -140,7 +161,7 @@ class crawler:
                 self.unable_5.append(href)
                 continue
             else:
-                GuDongInfo['企业名称'] = [href['company_name']] * GuDong_size
+                GuDongInfo['企业名称'] = [href['company_name']] * total_len
                 GuDongInfo['股东(发起人)'] = gudongs
                 GuDongInfo['持股比例'] = chigubilis
                 if sign == 0:
@@ -179,7 +200,8 @@ class crawler:
             f.close()
         self.GetHolderInfo(deal, 1)
 
-        if len(self.unable_4):
+        if len(self.unable_4) and self.DealTimeOut_max_times <= 5:
+            self.DealTimeOut_max_times += 1
             self.DealTimeOut()
 
     def DealUnKnown(self):
@@ -193,7 +215,8 @@ class crawler:
             f.close()
         self.GetHolderInfo(deal, 1)
 
-        if len(self.unable_5):
+        if len(self.unable_5) and self.DealUnKnown_max_times <= 5:
+            self.DealUnKnown_max_times += 1
             self.DealUnKnown()
 
 
